8e330a1143028e486b48091519761c62f5496d96
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-10-25 16:15:07 -0600

    feat(`WorkSection.jsx`): Create the component
    Create the WorkSection component that allows to add multiple work
    experiences. Use the same logic as in EducationSection component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './styles/App.css'
 import { General } from './components/General'
 import { EducationSection } from './components/EducationSection'
-import { Work } from './components/Work'
+import { WorkSection } from './components/WorkSection'
 
 function App() {
   return (
     <>
       <General />
       <EducationSection />
-      <Work />
+      <WorkSection />
     </>
   )
 }
