4c83a01a1eca8903523326e1138c6faebc8c50df
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-10-26 12:19:41 -0600

    feat(`TechnicalSkills.jsx`): Provide Add function
    Provide the add skills group functionality to the component using the
    addSkillsGroup() function called by a button of the same name.

--- src/components/TechnicalSkills.jsx
+++ src/components/TechnicalSkills.jsx
@@ -1,5 +1,104 @@
 import { useState } from "react";
 
 export function TechnicalSkills() {
-    
+  const [skills, setSkills] = useState([]);
+  const [skillsGroup, setSkillsGroup] = useState({
+    id: 0,
+    skillsType: "",
+    skillsList: "",
+  });
+  const [displayState, setDisplayState] = useState("form");
+
+  function handleSubmit() {
+    setDisplayState("resume");
+  }
+
+  function addSkillsGroup(limit) {
+    if (
+      skills.length < limit &&
+      skillsGroup.skillsType !== "" &&
+      skillsGroup.skillsList !== ""
+    ) {
+      setSkills([
+        ...skills,
+        {
+          id: crypto.randomUUID(),
+          skillsType: skillsGroup.skillsType,
+          skillsList: skillsGroup.skillsList,
+        },
+      ]);
+    }
+    setSkillsGroup({
+      id: 0,
+      skillsType: "",
+      skillsList: "",
+    });
+  }
+
+  
+
+  if (displayState === "form") {
+    return (
+      <>
+        <form className="technical-skills-form" onSubmit={() => handleSubmit()}>
+          <label htmlFor="skills-type">Enter skills type: </label>
+          <input
+            type="text"
+            id="skills-type"
+            value={skillsGroup.skillsType}
+            onChange={(e) =>
+              setSkillsGroup({ ...skillsGroup, skillsType: e.target.value })
+            }
+          />
+          <label htmlFor="skills-list">Enter list of skills: </label>
+          <input
+            type="text"
+            id="skills-list"
+            value={skillsGroup.skillsList}
+            onChange={(e) =>
+              setSkillsGroup({ ...skillsGroup, skillsList: e.target.value })
+            }
+          />
+          <button
+            type="button"
+            onClick={() => addSkillsGroup(5)}
+            id="add-skills-group"
+          >
+            Add Skills Group
+          </button>
+          <button
+            type="button"
+            onClick={() => updateSkillsGroup()}
+            id="update-skills-group"
+          >
+            Update Skills Group
+          </button>
+          <button type="submit">Submit</button>
+        </form>
+        <div>
+          {skills.map((skillsGroup) => (
+            <div key={skillsGroup.id}>
+              {skillsGroup.skillsType}: {skillsGroup.skillsList}
+              <button
+                type="button"
+                id="edit-skills-group-btn"
+                onClick={() => editSkillsGroup(skillsGroup.id)}
+              >
+                Edit Skills Group
+              </button>
+              <button
+                type="button"
+                id="delete-skills-group-btn"
+                onClick={() => deleteSkillsGroup(skillsGroup.id)}
+              >
+                Delete Skills Group
+              </button>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+
+
 }
