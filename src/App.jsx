e880465b412230a0b3ffd666420b543b6054d203
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-10-26 12:19:41 -0600

    feat(`TechnicalSkills.jsx`): Provide Add function
    Provide the add skills group functionality to the component using the
    addSkillsGroup() function called by a button of the same name.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './styles/App.css'
 import { General } from './components/General'
 import { EducationSection } from './components/EducationSection'
 import { WorkSection } from './components/WorkSection'
+import { TechnicalSkills } from './components/TechnicalSkills'
 
 function App() {
   return (
     <>
       <General />
       <EducationSection />
+      <TechnicalSkills />
       <WorkSection />
     </>
   )
