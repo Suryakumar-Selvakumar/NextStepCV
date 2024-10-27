3fa9b1ef0746d0bc9f56f0e532bda562e992fc7a
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-10-27 16:07:19 -0600

    feat(`GeneralResume.jsx`): Create the component
    Refactor General component the same way and retrieve the GeneralResume
    component from it.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       {/* <General /> */}
-      <EducationSection />
+      {/* <EducationSection /> */}
       {/* <TechnicalSkills /> */}
       {/* <WorkSection /> */}
       {/* <ProjectSection /> */}
