4f4dd4387cc8459f15559cf4542e5449cfa892c9
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-11-02 20:06:03 -0600

    feat(`Header.jsx`): Return the needed jsx
    Add all the needed JSX that have to be returned to the return statement
    of the component function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
+import "../styles/Header.css";
+
 export function Header() {
-    return (
-        <header>
-            
-        </header>
-    );
+  return (
+    <header>
+      <div className="logo">
+        <img src="./public/nextGold.svg" alt="logo of the app" id="logo-img" />
+        <p id="logo-text">NextStepCV</p>
+      </div>
+      <div className="header-buttons">
+        <button>Load Dummy</button>
+        <button>Clear Resume</button>
+        <button>Download</button>
+      </div>
+    </header>
+  );
 }
