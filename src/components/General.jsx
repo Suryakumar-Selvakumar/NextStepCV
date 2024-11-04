f3e7f9467c33b0a3deb632452b79fd198f031915
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-11-04 13:23:22 -0700

    refactor(`General.jsx`): Update dropdown logic
    Update the dropdown logic to use state and transitions instead of just
    class toggling and animations

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -17,6 +17,7 @@ export function General() {
           formSubmitted: false,
         }
   );
+  const [mainVisible, setMainVisible] = useState(false);
   const editGeneralDetailsBtn = useRef(null);
   const generalForm = useRef(null);
   const generalMainDiv = useRef(null);
@@ -60,36 +61,24 @@ export function General() {
     <div className="general">
       <div
         className="general-header"
-        onClick={(e) => {
-          if (e.target === e.currentTarget) {
-            if (generalMainDiv.current.classList.contains("visible")) {
-              generalMainDiv.current.classList.add("not-visible");
-              generalMainDiv.current.classList.remove("visible");
-            } else {
-              generalMainDiv.current.classList.remove("not-visible");
-              generalMainDiv.current.classList.add("visible");
-            }
-            dropDownSvg.current.classList.toggle("rotate-dropdown");
-          }
+        onClick={() => {
+          setMainVisible(!mainVisible);
+          dropDownSvg.current.classList.toggle("rotate-dropdown");
         }}
       >
         <h2
-          onClick={(e) => {
-            if (e.target === e.currentTarget) {
-              generalMainDiv.current.classList.toggle("visible");
-              dropDownSvg.current.classList.toggle("rotate-dropdown");
-            }
+          onClick={() => {
+            setMainVisible(!mainVisible);
+            dropDownSvg.current.classList.toggle("rotate-dropdown");
           }}
         >
           General Information
         </h2>
         <svg
           ref={dropDownSvg}
-          onClick={(e) => {
-            if (e.target === e.currentTarget) {
-              generalMainDiv.current.classList.toggle("visible");
-              dropDownSvg.current.classList.toggle("rotate-dropdown");
-            }
+          onClick={() => {
+            setMainVisible(!mainVisible);
+            dropDownSvg.current.classList.toggle("rotate-dropdown");
           }}
           className="dropdown-svg"
           xmlns="http://www.w3.org/2000/svg"
@@ -101,90 +90,96 @@ export function General() {
           />
         </svg>
       </div>
-      <div className="general-main not-visible" ref={generalMainDiv}>
-        <button
-          id="edit-general-details"
-          ref={editGeneralDetailsBtn}
-          onClick={() => handleEdit()}
+        <div
+          className={mainVisible ? "general-main visible" : "general-main"}
+          ref={generalMainDiv}
         >
-          {contactDetails.formSubmitted ? "Edit Details" : "Add Details"}
-        </button>
-        <form
-          className="general-form"
-          ref={generalForm}
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <label htmlFor="name">Name: </label>
-          <input
-            type="text"
-            id="name"
-            value={contactDetails.name}
-            onChange={(e) =>
-              setContactDetails({ ...contactDetails, name: e.target.value })
-            }
-            required
-          />
-          <label htmlFor="ph-no">Phone Number: </label>
-          <input
-            type="tel"
-            id="ph-no"
-            value={contactDetails.phNo}
-            onChange={(e) =>
-              setContactDetails({ ...contactDetails, phNo: e.target.value })
-            }
-            required
-          />
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            id="email"
-            value={contactDetails.email}
-            onChange={(e) =>
-              setContactDetails({ ...contactDetails, email: e.target.value })
-            }
-            required
-          />
-          <label htmlFor="linked-in">LinkedIn: </label>
-          <input
-            type="text"
-            id="linked-in"
-            value={contactDetails.linkedIn}
-            onChange={(e) =>
-              setContactDetails({ ...contactDetails, linkedIn: e.target.value })
-            }
-            required
-          />
-          <label htmlFor="github">GitHub: </label>
-          <input
-            type="text"
-            id="github"
-            value={contactDetails.gitHub}
-            onChange={(e) =>
-              setContactDetails({ ...contactDetails, gitHub: e.target.value })
-            }
-            required
-          />
           <button
-            type="button"
-            id="cancel-general"
-            onClick={() => handleCancel()}
+            id="edit-general-details"
+            ref={editGeneralDetailsBtn}
+            onClick={() => handleEdit()}
           >
-            Cancel
+            {contactDetails.formSubmitted ? "Edit Details" : "Add Details"}
           </button>
-          <button type="submit" id="submit-general">
-            Save
-          </button>
-        </form>
-        {contactDetails.formSubmitted && (
-          <div className="contact-details-card">
-            <p>{contactDetails.name}</p>
-            <p>{contactDetails.phNo}</p>
-            <p>{contactDetails.email}</p>
-            <p>{contactDetails.linkedIn}</p>
-            <p>{contactDetails.gitHub}</p>
-          </div>
-        )}
-      </div>
+          <form
+            className="general-form"
+            ref={generalForm}
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              id="name"
+              value={contactDetails.name}
+              onChange={(e) =>
+                setContactDetails({ ...contactDetails, name: e.target.value })
+              }
+              required
+            />
+            <label htmlFor="ph-no">Phone Number: </label>
+            <input
+              type="tel"
+              id="ph-no"
+              value={contactDetails.phNo}
+              onChange={(e) =>
+                setContactDetails({ ...contactDetails, phNo: e.target.value })
+              }
+              required
+            />
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              id="email"
+              value={contactDetails.email}
+              onChange={(e) =>
+                setContactDetails({ ...contactDetails, email: e.target.value })
+              }
+              required
+            />
+            <label htmlFor="linked-in">LinkedIn: </label>
+            <input
+              type="text"
+              id="linked-in"
+              value={contactDetails.linkedIn}
+              onChange={(e) =>
+                setContactDetails({
+                  ...contactDetails,
+                  linkedIn: e.target.value,
+                })
+              }
+              required
+            />
+            <label htmlFor="github">GitHub: </label>
+            <input
+              type="text"
+              id="github"
+              value={contactDetails.gitHub}
+              onChange={(e) =>
+                setContactDetails({ ...contactDetails, gitHub: e.target.value })
+              }
+              required
+            />
+            <button
+              type="button"
+              id="cancel-general"
+              onClick={() => handleCancel()}
+            >
+              Cancel
+            </button>
+            <button type="submit" id="submit-general">
+              Save
+            </button>
+          </form>
+          {contactDetails.formSubmitted && (
+            <div className="contact-details-card">
+              <p>{contactDetails.name}</p>
+              <p>{contactDetails.phNo}</p>
+              <p>{contactDetails.email}</p>
+              <p>{contactDetails.linkedIn}</p>
+              <p>{contactDetails.gitHub}</p>
+            </div>
+          )}
+        </div>
     </div>
   );
 }
