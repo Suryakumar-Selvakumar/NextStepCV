f820e86f6be4a5baec310bc9b68ac1af1e303cd9
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-11-07 14:50:03 -0700

    feat(`Resume.jsx`): Setup and style JSX for resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,3 +1,11 @@
+import "../styles/Resume.css";
+
 export function Resume() {
-    
+  return (
+    <div className="resume-container">
+      <div className="resume">
+        
+      </div>
+    </div>
+  );
 }
