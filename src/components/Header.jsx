b88833b4431d91f165bb6edbab293a3af668dd39
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-11-12 14:26:57 -0700

    fix: Fix minor DOM & CSS issues in mobile layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -284,7 +284,7 @@ export function Header({ appData, setAppData }) {
                 d="M14,2L20,8V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H14M18,20V9H13V4H6V20H18M12,19L8,15H10.5V12H13.5V15H16L12,19Z"
               />
             </svg>
-            <span>Download</span>
+            <span className="button-text">Download</span>
           </PDFDownloadLink>
         </div>
       </header>
